ba37a4ce2e1612c41836694ea6515328ab6b1562
Luke Todd <luketodd@zoho.com>
2020-01-26 11:47:04 +0000

    Attempting to get now deployment working

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -10,6 +11,15 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+if (process.env.NODE_ENV === 'production') {
+  const staticPath = path.resolve(__dirname, 'client', 'build');
+  app.use(express.static(staticPath));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(staticPath, 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
